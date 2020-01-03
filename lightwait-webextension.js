c629fa01c9489377e252eeb917380f13ad688ddc
Bastian Gebhardt <Bastian.Gebhardt@jambit.com>
2020-01-03 20:35:57 +0100

    Add blinking and multiblinking support

--- lightwait-webextension.js
+++ lightwait-webextension.js
@@ -5,13 +5,42 @@ const port = chrome.runtime.connectNative('lightwait');
 
 let useCanvas = false;
 
+let timeoutId = null;
+let colorAmount = 1;
+let currentColorIndex = -1;
+
 console.log('connected...');
 
 port.onMessage.addListener(message => {
     if (message.error) {
         console.error('Error: ', message.error);
     } else {
-        chrome.browserAction.setIcon({imageData: createIcon(convertColor(message))});
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+            timeoutId = null;
+        }
+        const {blink, colors} = extractColors(message)
+
+        if (blink || colors.length > 1) {
+
+            if (blink && colors.length === 1) {
+                colors.push('#000')
+            }
+
+            colorAmount = colors.length
+
+            // value is incremented before accessing array, so start with one less
+            currentColorIndex = -1;
+
+            timeoutId = setInterval(() => {
+                currentColorIndex = (currentColorIndex + 1) % colorAmount;
+
+                const newColor = colors[currentColorIndex];
+                chrome.browserAction.setIcon({imageData: createIcon(newColor)});
+            }, 600);
+        } else {
+            chrome.browserAction.setIcon({imageData: createIcon(colors[0])});
+        }
     }
 })
 
@@ -35,6 +64,26 @@ function createIcon(color) {
     ctx.fill();
 
     return ctx.getImageData(0, 0, ICON_SIZE, ICON_SIZE);
+}
+
+function extractColors(colorsString) {
+    const blink = colorsString.startsWith('b')
+
+    if (blink) {
+        colorsString = colorsString.substring(1)
+    }
+
+    return {
+        blink,
+        colors: colorsString
+            .split('|')
+            .map(colorString => '#'.concat(colorString.split(':')
+                                   .map(baseColor => (parseInt(baseColor) || 0).toString(16))
+                                   .map(c => c.length === 1 ? `0${c}` : c)
+                                   .join('')
+            )
+        )
+    }
 }
 
 function convertColor(colorString) {
